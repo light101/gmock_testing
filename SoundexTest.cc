7e267607a47afbc2e02d7ade7196c1c0f8b08394
light1 <valeriprodanov@yahoo.com>
2016-02-09 21:38:27 +0200

    Initial

---
+++ SoundexTest.cc
@@ -0,0 +1,9 @@
+#include <gmock/gmock.h>
+
+class Soundex {
+};
+
+TEST(SoundexEncoding, RetainSoleLetterOfOneLetterWord) {
+   Soundex soundex;
+}
+
---
+++ build/CMakeFiles/SoundExTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/SoundExTest.dir/SoundexTest.cc.o"
+  "SoundExTest.pdb"
+  "SoundExTest"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/SoundExTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
